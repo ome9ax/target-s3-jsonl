b3b5f85185e1575b972b9f097d6435fb3b5223b7
Eddy Delta <eddy.delta@marshmallow.co>
2021-08-08 11:45:56 +0100

    [Test Genesis] test get_target_key dict message

--- tests/test_main.py
+++ tests/test_main.py
@@ -7,7 +7,7 @@ from target_s3_jsonl import get_target_key, persist_lines
 
 def test_get_target_key():
     '''TEST : simple persist_lines call'''
-    assert get_target_key('dummy_stream', naming_convention='{stream}-{timestamp}.jsonl', timestamp='99') == 'dummy_stream-99.jsonl'
+    assert get_target_key({'stream': 'dummy_stream'}, naming_convention='{stream}-{timestamp}.jsonl', timestamp='99') == 'dummy_stream-99.jsonl'
 
 
 def test_persist_lines():
