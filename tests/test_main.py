849bdbdd92ccde0fea9ffa99b962d21f36032758
Eddy Delta <eddy.delta@marshmallow.co>
2021-08-08 11:18:52 +0100

    [Test Genesis] dry test run

---
+++ tests/test_main.py
@@ -0,0 +1,10 @@
+'''Tests for the target_s3_jsonl.main module'''
+# Standard library imports
+
+# Reader imports
+from target_s3_jsonl import persist_lines
+
+
+def test_persist_lines():
+    '''TEST : simple persist_lines call'''
+    assert True
