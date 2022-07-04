5ce747f76c7c7719d30e4b7d0c1922a0032bd80d
Hal Ali <haleemur@users.noreply.github.com>
2022-07-04 18:06:13 -0400

    add support for assuming a role via `role_arn`
    if `role_arn` is specified, first a session is created using this role, and then an s3 client is created with the session in the assumed role.

--- target_s3_jsonl/s3.py
+++ target_s3_jsonl/s3.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import os
+import time
 import backoff
 import boto3
 from botocore.exceptions import ClientError
@@ -32,7 +33,10 @@ def create_client(config):
     aws_session_token = config.get('aws_session_token') or os.environ.get('AWS_SESSION_TOKEN')
     aws_profile = config.get('aws_profile') or os.environ.get('AWS_PROFILE')
     aws_endpoint_url = config.get('aws_endpoint_url')
-
+    role_arn = config.get('role_arn')
+    
+    endpoint_parmas = {'endpoint_url': aws_endpoint_url} if aws_endpoint_url else {}
+    
     # AWS credentials based authentication
     if aws_access_key_id and aws_secret_access_key:
         aws_session = boto3.session.Session(
@@ -42,12 +46,18 @@ def create_client(config):
     # AWS Profile based authentication
     else:
         aws_session = boto3.session.Session(profile_name=aws_profile)
-
-    if aws_endpoint_url:
-        return aws_session.client('s3', endpoint_url=aws_endpoint_url)
-    else:
-        return aws_session.client('s3')
-
+    if role_arn:
+        role_name = role_arn.split('/', 1)[1]
+        sts = aws_session.client('sts', **endpoint_params)
+        resp = sts.assume_role(RoleArn=role_arn, RoleSessionName=f'Meltano-{role_name}-profile:{aws_profile}-{int(time.time())}')
+        credentials = {
+            "aws_access_key_id": resp["Credentials"]["AccessKeyId"],
+            "aws_secret_access_key": resp["Credentials"]["SecretAccessKey"],
+            "aws_session_token": resp["Credentials"]["SessionToken"],
+        }
+        aws_session = boto3.Session(**credentials)
+    return aws_session.client('s3', **endpoint_params)
+    
 
 # pylint: disable=too-many-arguments
 @retry_pattern()
