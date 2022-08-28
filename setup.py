f209e5e9ce41b9297a834fc6bef8e0f65b562bbb
Eddy Delta <eddy.delta@marshmallow.co>
2022-08-28 01:57:20 +0100

    [target-core] S3 module: bump

--- setup.py
+++ setup.py
@@ -4,7 +4,6 @@ from setuptools import setup
 
 setup(
     install_requires=[
-        # 'target-core==0.0.5'
-        'target-core @ git+ssh://git@gitlab.com/singer-core/target-core@13-rest-api-support#egg=target-core'
+        'target-core==0.0.5'
     ]
 )
