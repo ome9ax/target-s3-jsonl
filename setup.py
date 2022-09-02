ef5d3f700a706fd2e56f864cd1be7e8bcd837ea9
Eddy Delta <eddy.delta@marshmallow.co>
2022-09-02 19:23:38 +0100

    [target-core] target-core bump 0.0.5 -> 0.0.6

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     install_requires=[
-        'target-core==0.0.5',
+        'target-core==0.0.6',
         'jsonschema==4.14.0',
         'boto3==1.24.57',
         'backoff==2.1.2'
